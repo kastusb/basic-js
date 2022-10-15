ea3a0fb48e2ef08e72f7a83273126ebd7e3aebbc
kastusb <103989544+kastusb@users.noreply.github.com>
2022-10-15 09:49:13 +0300

    add count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function countCats(matrix) {
+  if (matrix.length === 0) return 0;
+  let result = matrix.reduce((a,b) => a.concat(b).filter(x => x === '^^'));
+  return result.length;
 }
 
 module.exports = {
